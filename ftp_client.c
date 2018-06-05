8ec2498fcc2a760ee6bc475d10788e76214cd661
salamander <1906747819@qq.com>
2018-06-05 13:46:56 +0800

    gbk编码转utf-8

--- ftp_client.c
+++ ftp_client.c
@@ -10,6 +10,7 @@
 #include <stdbool.h>
 #include <fcntl.h>
 #include <pwd.h>
+#include <iconv.h>
 
 
 #define FTP_SERVER_PORT 21 
@@ -34,6 +35,29 @@ bool is_correct_respond(const char *respond, int code); // 是否返回正确响
 unsigned int cal_data_port(const char *recv_buffer);
 int get_client_data_socket(unsigned int client_cmd_port);
 void connect_server(int socket, const char *server_ip, unsigned int server_port);
+
+int code_convert(const char *from_charset, const char *to_charset, char *inbuf, size_t inlen, char *outbuf, size_t outlen)  
+{
+    iconv_t cd;
+    int rc;  
+    char **pin = &inbuf;
+    char **pout = &outbuf;  
+    
+    cd = iconv_open(to_charset, from_charset);  
+    if (cd==0)  
+            return -1;
+    memset(outbuf, 0, outlen);  
+    if (iconv(cd, pin, &inlen, pout, &outlen) == -1)  
+            return -1;  
+    iconv_close(cd);
+    return 0;  
+}  
+
+
+int g2u(char *inbuf, size_t inlen, char *outbuf, size_t outlen)  
+{
+    return code_convert("gb2312", "utf-8", inbuf, inlen, outbuf, outlen);  
+}
 
 void set_flag(int, int);
 void clr_flag(int, int);
@@ -166,7 +190,12 @@ int main(int argc, char **argv)
                     char data_buffer[4000];
                     bzero(data_buffer, 4000);
                     int length = recv(client_data_socket, data_buffer, 4000, 0);
-                    printf("%s\n", data_buffer);
+
+                    char *ptr = (char *)calloc(sizeof(data_buffer), sizeof(char));
+                    g2u(data_buffer, strlen(data_buffer), ptr, sizeof(data_buffer));
+
+                    printf("%s\n", ptr);
+                    free(ptr);
                     close(client_data_socket);
                     exit(0);
                 } else {
